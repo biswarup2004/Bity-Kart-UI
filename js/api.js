79cdff8f457982f099730b6a523c4c9ebf5e0f9c
Biswarup <biswarupmaity2004official@gmail.com>
2025-12-19 18:27:02 +0530

    changed

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@ function getAuthHeaders() {
     const token = localStorage.getItem('token');
     const headers = {
         'Content-Type': 'application/json'
-    };
+    }; 
     
     if (token) {
         headers['Authorization'] = `Bearer ${token}`;
